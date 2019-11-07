2d8b95cc0128ddfda2d45b2e737e97316c0244f6
begna002 <begna002@umn.edu>
2019-11-07 13:15:02 -0600

    adding pa3

---
+++ pa3/header.h
@@ -0,0 +1,23 @@
+/*test machine: CSELAB_machine_name * date: mm/dd/yy
+* name: full_name1 , [full_name2]
+* x500: id_for_first_name , [id_for_second_name] */
+
+/*
+header.h, header for all source files
+it will: 
+- structure definition
+- global variable, lock declaration (extern)
+- function declarations
+*/
+
+#ifndef _HEADER_H_
+#define _HEADER_H_
+
+// header here..
+
+
+
+
+
+
+#endif
---
+++ pa3/utils.c
@@ -0,0 +1,12 @@
+/*test machine: CSELAB_machine_name * date: mm/dd/yy
+* name: full_name1 , [full_name2]
+* x500: id_for_first_name , [id_for_second_name] */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "header.h"
+// pthread.h included in header.h
+
+// implement shared queue, final histogram here.. 
+
