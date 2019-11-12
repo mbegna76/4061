91c42376e6acb73c50cd38dc44c86b717f94d560
Wally <luxx0460@umn.edu>
2019-11-12 16:42:09 -0600

    error handling

--- pa3/main.c
+++ pa3/main.c
@@ -9,16 +9,40 @@
 
 int main(int argc, char** argv) {
 
- // Initializing condBuffer
- initializeDict();
- struct condBuffer* cq = (struct condBuffer*) malloc(sizeof(struct condBuffer));
- cq->filename = argv[2];
- cq->num_items = 0;
- cq->queue = createQueue();
-	cq->cond = (pthread_cond_t*) malloc(sizeof(pthread_cond_t));
+  // Initializing condBuffer
+  initializeDict();
+  struct condBuffer* cq = (struct condBuffer*) malloc(sizeof(struct  condBuffer));
+  cq->filename = argv[2];
+  cq->num_items = 0;
+  cq->queue = createQueue();
+  cq->cond = (pthread_cond_t*) malloc(sizeof(pthread_cond_t));
 	cq->mutex = (pthread_mutex_t*) malloc(sizeof(pthread_mutex_t));
 	pthread_cond_init(cq->cond, NULL);
 	pthread_mutex_init(cq->mutex, NULL);
 
+  if (argc < 3 ) { // passes through too little arguments
+    printf("Too little arguments given");
+  }
+  if (argc == 3 && argv[1] <= 0) { // passes through standard arguments
+    cq->logFlag = 0;
+    // do the thing 
+  }
+  if (argc == 4 ) { // passes through standard arguments + flag
+    if (strncmp(argv[3], "-p", 2)) { // 
+      cq->logFlag = 1; 
+    }
+    else {
+      printf("Please use a valid option flag");
+      exit(1);
+    }
+  }
+  if (argc >= 5) { // passes through too many arguments 
+    printf("Too many arguments given");
+    exit(0);
+  }
+
  return 0;
 }
+
+
+
